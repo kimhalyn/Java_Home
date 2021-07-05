eae4c7394b141bb2d1d01c46999dd02cbe6996e1
khl <reedweed@naver.com>
2021-07-06 02:33:22 +0900

    ch01

---
+++ ch01/src/Ex1_01.java
@@ -0,0 +1,33 @@
+/*
+ * 날짜: 2021/05/04  
+ * 내용: 자바의 정석_이클립스 단축키
+ * 이름: 김하린
+ * 
+ * ctrl + shift + L : 단축키 전체 보기
+ * ctrl + (+, -) : 글자 크기 조절
+ * ctrl + D : 한 줄 삭제
+ * ctrl + alt + down : 행 단위 복사(window 단축키와 충돌)
+ * -> window -> preferences -> General -> Keys -> copy line 검색 -> ctrl + alt + shift + down 으로 변경
+ * alt + up/down : 행 단위 이동
+ * ctrl + / : 주석처리 
+ * ctrl + space: 자동완성
+ * ctrl + shift + F: 자동정렬
+ * 
+ * ctrl + shift + enter: 커서 위에 빈줄 삽입 
+ * alt + shift + A : 멀티 컬럼 편집 (커서가 십자가 모양으로 변함 -> 변경하고자 하는 곳 클릭 -> shift + down)
+ * ctrl + shift + o : 자동으로 import문 생성
+ */
+
+public class Ex1_01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+	}
+
+}
---
+++ ch01/src/Ex1_02.java
@@ -0,0 +1,21 @@
+/*
+ * 날짜: 2021/05/04 
+ * 내용: 자바의 정석_글자 출력, 사칙연산 실행
+ * 이름: 김하린
+ */
+public class Ex1_02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("3+5"); // ""따옴표 안의 숫자는 문자로 인식
+		System.out.println(3+5);
+		System.out.println(3-5);
+		System.out.println(3*5);
+		System.out.println(3/5);
+		System.out.print("Hello1");
+		System.out.println("Hello2");
+		System.out.print("Hello3");
+		System.out.println("Hello4");
+	}
+
+}
